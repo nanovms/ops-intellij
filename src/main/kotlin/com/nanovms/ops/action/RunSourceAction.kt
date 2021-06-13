918c615aadb42f9461fc77314fac9a9a30db6ca0
Lufty <lufty.abdillah@gmail.com>
2021-06-14 01:23:01 +0700

    Fixed run source file to support output streaming

---
+++ src/main/kotlin/com/nanovms/ops/action/RunSourceAction.kt
@@ -0,0 +1,53 @@
+package com.nanovms.ops.action
+
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.components.service
+import com.intellij.openapi.fileEditor.FileDocumentManager
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.nanovms.ops.Log
+import com.nanovms.ops.Service
+import com.nanovms.ops.command.Command
+import com.nanovms.ops.command.CommandListener
+import com.nanovms.ops.command.RunSourceCommand
+import com.nanovms.ops.command.UnsupportedSourceType
+
+class RunSourceAction : BaseAction() {
+    override fun isEnabled(e: AnActionEvent, ops: Service): Boolean {
+        if (e.project == null) {
+            return false
+        }
+        val editor = FileEditorManager.getInstance(e.project!!).selectedTextEditor
+        return (editor != null)
+    }
+
+    override fun actionPerformed(e: AnActionEvent) {
+        e.project?.let {
+            val editor = FileEditorManager.getInstance(e.project!!).selectedTextEditor
+            if (editor != null) {
+                var file = FileDocumentManager.getInstance().getFile(editor.document)
+                if (file != null) {
+                    if (file.extension == null) {
+                        Log.errorAndNotify("Unknown file extension: ${file.path}")
+                        return
+                    }
+
+                    try {
+                        val command = RunSourceCommand(it, file).withListener(
+                            object : CommandListener() {
+                                override fun started(cmd: Command) {
+                                    Log.infoAndNotify("[exec] ${file.path}")
+                                }
+                            }
+                        )
+                        command.execute()
+                    } catch(ex: UnsupportedSourceType) {
+                        val ops = service<Service>()
+                        ex.message?.let {
+                            msg -> ops.println(it, msg)
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/nanovms/ops/command/RunSourceCommand.kt
@@ -0,0 +1,22 @@
+package com.nanovms.ops.command
+
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.vfs.VirtualFile
+import java.io.File
+
+class RunSourceCommand(project: Project, private val file: VirtualFile) : Command(project) {
+
+
+    override fun createArguments(): Collection<String> {
+        when (file.extension) {
+            "js" -> {
+                return listOf("pkg", "load", "node_v14.2.0", "-a", file.path)
+            }
+            "cpp" -> {
+                val binpath = file.parent.path + File.pathSeparatorChar + file.name
+                return listOf("g++", file.path, "-o", binpath)
+            }
+        }
+        throw UnsupportedSourceType(file)
+    }
+}
