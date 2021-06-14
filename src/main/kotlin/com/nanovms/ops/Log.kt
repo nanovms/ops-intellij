089fbeba01d315451dc7ab8e0ce5d1c15d1746c1
Lufty <lufty.abdillah@gmail.com>
2021-06-15 05:30:51 +0700

    Removed unused functions

--- src/main/kotlin/com/nanovms/ops/Log.kt
+++ src/main/kotlin/com/nanovms/ops/Log.kt
@@ -8,39 +8,13 @@ import com.intellij.openapi.project.ProjectManager
 
 class Log {
     companion object {
-        private val logger = Logger.getInstance("NanoVMs")
-        private val notificationGroup = NotificationGroupManager.getInstance().getNotificationGroup("NanoVMs")
-
-        fun info(message: String) {
-            val msg = "[NanoVMs]: $message"
-            logger.info(msg)
-            println(msg)
-        }
-
-        fun error(message: String) {
-            val msg = "[NanoVMs] ERR: $message"
-            logger.error(msg)
-            println(msg)
-        }
-
-        fun error(err: Throwable?) {
-            val msg = "[NanoVMs] ERR: $err"
-            logger.error(msg)
-            println(msg)
-        }
-
-        fun infoAndNotify(message: String) {
+       fun infoAndNotify(message: String) {
             info(message)
             notifyInfo(message)
         }
 
         fun errorAndNotify(message: String) {
             error(message)
-            notifyError(message)
-        }
-
-        fun errorAndNotify(message: String, err: Throwable?) {
-            error("$message: $err")
             notifyError(message)
         }
 
@@ -52,6 +26,18 @@ class Log {
             notify(message, NotificationType.ERROR)
         }
 
+        private fun error(message: String) {
+            val msg = "[NanoVMs] ERR: $message"
+            logger.error(msg)
+            println(msg)
+        }
+
+        private fun info(message: String) {
+            val msg = "[NanoVMs]: $message"
+            logger.info(msg)
+            println(msg)
+        }
+
         private fun notify(message: String, type: NotificationType) {
             var notif = notificationGroup.createNotification(message, type)
             val projects = ProjectManager.getInstance().openProjects
@@ -61,5 +47,8 @@ class Log {
             }
             notif.notify(project)
         }
+
+        private val logger = Logger.getInstance("NanoVMs")
+        private val notificationGroup = NotificationGroupManager.getInstance().getNotificationGroup("NanoVMs")
     }
 }
