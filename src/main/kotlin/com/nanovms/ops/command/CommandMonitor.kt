8281bbf86cf5fb5a6e8503d3848fd3b195dbe98b
Lufty <lufty.abdillah@gmail.com>
2021-06-15 12:15:41 +0700

    Catch ioexception if input stream closed earlier before the live flag

--- src/main/kotlin/com/nanovms/ops/command/CommandMonitor.kt
+++ src/main/kotlin/com/nanovms/ops/command/CommandMonitor.kt
@@ -4,6 +4,7 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.nanovms.ops.Service
 import java.io.BufferedReader
+import java.io.IOException
 import java.io.InputStreamReader
 
 class CommandMonitor(private val project: Project, private val command: Command) : Thread() {
@@ -12,9 +13,15 @@ class CommandMonitor(private val project: Project, private val command: Command)
         val reader = BufferedReader(InputStreamReader(command.inputStream))
         var line: String?
         while (command.isAlive) {
-            line = reader.readLine()
-            line?.let {
-                ops.println(project, it)
+            try {
+                line = reader.readLine()
+                line?.let {
+                    ops.println(project, it)
+                }
+            } catch (ex: IOException) {
+                // At this point stream already closed by the process,
+                // but live flag is not updated yet, so just ignore ..
+                // and the loop will break by itself
             }
         }
         ops.releaseCommand(command)
